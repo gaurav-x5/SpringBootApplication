08b7a17b5c0cb1d79ef22693423f0bfbc0997412
gaurav-x5 <gaurav.sit.cse@gmail.com>
2019-11-08 02:50:48 +0530

    SBP:3 returning a bean back instead of hardcoded values

--- src/main/java/SB_basic/basic/HelloWorldController.java
+++ src/main/java/SB_basic/basic/HelloWorldController.java
@@ -1,5 +1,6 @@
 package SB_basic.basic;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @RestController
 public class HelloWorldController {
-    //GET
-    //URI -/hello-world
-    //method - "Hello World"
+
     //we need to define this class as a controller
     //it should be handling all the http requests
-    @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    @GetMapping(path = "/hello-world")
     public String HelloWorld(){
         return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean HelloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
