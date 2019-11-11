ce0eda80cbed54a843d758666823de8dbc3b4fd7
gaurav-x5 <gaurav.sit.cse@gmail.com>
2019-11-12 01:47:32 +0530

    SBP: till validation

--- src/main/java/SB_basic/basic/user/UserResource.java
+++ src/main/java/SB_basic/basic/user/UserResource.java
@@ -1,9 +1,13 @@
 package SB_basic.basic.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Null;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,14 +23,31 @@ public class UserResource {
 
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user==null)
+            throw new UserNotFoundException("id-"+id);
+        return user;
     }
 
     //Created as a status code
     // input - details of user
     //output - Created and return the created URI
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user==null)
+            throw new UserNotFoundException("id-"+id);
+
     }
 }
